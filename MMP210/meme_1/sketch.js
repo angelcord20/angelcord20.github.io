987d0ae35a65f399de4d6327deab2076b5f97060
angelcord20 <54859227+angelcord20@users.noreply.github.com>
2020-03-03 14:34:20 -0500

    Update sketch.js

--- MMP210/meme_1/sketch.js
+++ MMP210/meme_1/sketch.js
@@ -1,45 +1,45 @@
 /*
- meme version 1 example
- 2.25.2020
+  meme version 1 example
+  2.25.2020
+*/
 
- */
+// global scope
+var fuegoImage;
 
- // global scope
- var fuegoImage;
+function preload() {
+  fuegoImage = loadImage("fuego.jpg");
+}
 
- function preload () {
-    fuegoImage = loadImage("fuego.jpg")
- }
+function setup() {
+  var canvas = createCanvas(500, 500);
+  canvas.drawingContext.miterLimit = 2;
+}
 
- function setup () {
-    var canvas = createCanvas(500, 500);
-    canvas.drawingContext.miterLimit = 2;
- }
-
- function draw() {
+function draw() {
   background('gray');
 
-  //draw the image
-  ting(255, 255, 255);
-  image(fuegoImage, 0, 0, width/2, height);
+  // draw the image
+  tint(255, 255, 255);
+  image(fuegoImage, 0, 0, 500, height);
 
-  tint(255, 0, 100);
-  image(catImage, width/2, 0, width/2, height);
+  //tint(255, 0, 100);
+  //image(fuegoImage, width/2, 0, width/2, height);
 
   var imageColor = get(mouseX, mouseY);
-  fill(imageColor)
+  fill(imageColor);
   noStroke();
   rect(mouseX, mouseY, 50, 50);
 
+
   textSize(100);
-  fill('blue');
-  stroke('pink');
+  fill('white');
+  stroke('black');
   strokeWeight(20);
   textStyle(ITALIC);
   textFont('Segoe UI');
   textAlign(LEFT, CENTER);
 
-  var hello = 'hello world'
+  var hello = '';
   var n = map(frameCount, 0, 400, 0, hello.length);
   text(hello.charAt(n), 0, 250);
 
@@ -48,7 +48,16 @@
   textStyle(NORMAL);
   textAlign(CENTER, TOP);
 
-  var welcome = 'welcome to my website hello';
-  var end = map(frameCount, 0, 200, 0, welcome.length);
-  text(welcome.substring(0, end), 250, 350);  
- }
+  var welcome = 'No one:';
+  var end = map(frameCount, 10, 200, 10, welcome.length);
+  text(welcome.substring(), 250, 10);
+
+  var welcome = 'Literally no one:';
+  var end = map(frameCount, 60, 200, 20, welcome.length);
+  text(welcome.substring(), 250, 37);
+
+    var welcome = 'Me in chemistry class:';
+  var end = map(frameCount, 1, 200, 1, welcome.length);
+  text(welcome.substring(0, end), 250, 67);
+
+}
